f89a61e34ee6402762cdc1bc78347ce123fdeb6e
Talik Kasozi <talik.aziizi@gmail.com>
2025-05-26 16:10:04 -0400

    - Fixing the pipeline

--- ManagedLibrary/build.sh
+++ ManagedLibrary/build.sh
@@ -3,19 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "1"
-pwd
-
-echo "2"
-
-ls -l
-
-echo "3"
-
-ls .. -l
-
-#rm -r publish bin obj ../Release/bin/plugins/managed
-
 # clean
 dotnet clean
 
@@ -27,3 +14,8 @@ dotnet build ./ManagedLibrary.sln --configuration Release --no-restore
 
 # Publish project
 dotnet publish -c Release -r win-x64 --no-self-contained -o "./publish"
+
+
+echo "2"
+
+ls -l ./publish
